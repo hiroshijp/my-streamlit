599ea8cff64f55db8458f618b539714bf4fadeb0
hiroshijp <138178456+hiroshijp@users.noreply.github.com>
2025-10-24 04:26:09 +0000

    fix: 見出しと説明を変更

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Welcome hiroshijp page")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This is the personal page of hiroshijp. Here you can find information about my projects and interests."
 )
